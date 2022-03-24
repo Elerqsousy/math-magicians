8acb71fd6ef82096076dfb10f8200eae64f07912
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-24 10:18:18 +0200

    Add home page component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HomePgae = () => (
+  <div>
+    <h2>Welcome to out page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+      recusandae unde at explicabo nobis ad ipsa totam quidem, natus, corporis
+      molestias veritatis repudiandae perspiciatis quaerat provident modi
+      commodi. Dolorem natus fuga et suscipit vel? Numquam corporis dolorum
+      autem esse vero corrupti dolorem consequatur hic amet similique?
+      Aspernatur ut repudiandae rerum fugiat illum inventore quam deserunt,
+      nihil temporibus dignissimos reprehenderit consectetur, veniam harum
+      animi beatae. Doloribus architecto nulla nihil, nobis quia eligendi quas
+      aspernatur deserunt maiores dolorem molestias labore, quibusdam repellat
+      possimus? Non magnam porro repellendus, vitae placeat ut? Illum possimus
+      facilis debitis iure quisquam animi impedit maiores minus amet expedita!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+      recusandae unde at explicabo nobis ad ipsa totam quidem, natus, corporis
+      molestias veritatis repudiandae perspiciatis quaerat provident modi
+      commodi. Dolorem natus fuga et suscipit vel? Numquam corporis dolorum
+      autem esse vero corrupti dolorem consequatur hic amet similique?
+      Aspernatur ut repudiandae rerum fugiat illum inventore quam deserunt,
+      nihil temporibus dignissimos reprehenderit consectetur, veniam harum
+      animi beatae. Doloribus architecto nulla nihil, nobis quia eligendi quas
+      aspernatur deserunt maiores dolorem molestias labore, quibusdam repellat
+      possimus? Non magnam porro repellendus, vitae placeat ut? Illum possimus
+      facilis debitis iure quisquam animi impedit maiores minus amet expedita!
+    </p>
+  </div>
+);
+
+export default HomePgae;
