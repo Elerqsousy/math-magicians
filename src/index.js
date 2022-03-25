c71489226800fad95a8a9b04881e8c211b0eb8d3
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-25 11:05:39 +0200

    Remove strict mode

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import "./calculator.css"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root'),
 );
